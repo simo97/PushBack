8a63168ac5fc8b580b1b2fa611369c11e36b628d
adonis simo <simoadonis@yahoo.fr>
2018-12-24 17:40:05 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -74,7 +74,7 @@ def notify(request):
         for cl in clients:
             print(cl)
             notification.clients.add(cl)  # connect the client to the notification
-        print(notification)
+        print(notification.clients.all())
 
         handle_notification(notification, True)
         return get_success_res('Notifications send with success with success')
